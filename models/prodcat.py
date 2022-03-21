5eecb3822af89cd25197535bcc364f3bcd87af60
MOHAMMED OUDAH <m-oudah@hotmail.com>
2022-03-21 16:36:06 +0300

    update namavet layout

---
+++ models/prodcat.py
@@ -0,0 +1,6 @@
+from odoo import models, fields, api
+
+
+class PRODCAT(models.Model):
+    _inherit = 'sale.order.line'
+    product_categ_id = fields.Selection(related='product_id.product_tmpl_id.type', string='Category')
