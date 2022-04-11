e667c39826bfdc7990c1b8c9bf2e11d722875f82
MOHAMMED OUDAH <m-oudah@hotmail.com>
2022-04-11 15:05:32 +0300

    update layouts

--- models/prodcat.py
+++ models/prodcat.py
@@ -3,4 +3,5 @@ from odoo import models, fields, api
 
 class PRODCAT(models.Model):
     _inherit = 'sale.order.line'
-    product_categ_id = fields.Selection(related='product_id.product_tmpl_id.type', string='Category')
+    # product_categ_id = fields.Selection(related='product_id.product_tmpl_id.type', string='Category')
+    product_categ_id = 555
