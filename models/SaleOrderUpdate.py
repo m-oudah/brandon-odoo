0d68c9f723b03f0a815d46db19ca9c0a30c7ac80
MOHAMMED OUDAH <m-oudah@hotmail.com>
2022-04-19 16:36:18 +0300

    update malven layout

--- models/SaleOrderUpdate.py
+++ models/SaleOrderUpdate.py
@@ -5,4 +5,7 @@ class SaleOrderUpdate(models.Model):
     _inherit = 'sale.order'
     origin_country = fields.Char(string='Origin Country')
     ship_to = fields.Char(string='Ship To')
+    ship_fees = fields.Integer(string='Shipping Fees')
+    # new_amount_total = ship_fees
+
     # custom_field = fields.Char(string='Custom Field')
